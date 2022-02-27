52d35c4cfef823a8ba1556cb5f16e42e043fa3a7
uzunali <aluz2412@gmail.com>
2022-02-27 20:53:17 +0000

    header

--- ipic_GUI.py
+++ ipic_GUI.py
@@ -1,3 +1,12 @@
+# -*- encoding: utf-8 -*-
+'''
+@Author  :   Ali Uzun 
+@Time    :   2022/02/27 20:52:35
+@Version :   1.0
+'''
+
+# here put the import lib
+
 from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
